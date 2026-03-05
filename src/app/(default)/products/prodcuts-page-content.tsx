312a65cc0fff550139f70e4acbc329b189dc7c88
Cross-Technology <crosscambodia@gmail.com>
2026-03-05 14:13:54 +0700

    first commit: initializing kfc dashboard

--- src/app/(default)/products/prodcuts-page-content.tsx
+++ src/app/(default)/products/prodcuts-page-content.tsx
@@ -7,6 +7,7 @@ import { Plus } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useMenuColumns } from "./columns"
 import { datas } from "@/app/(default)/products/data"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 
 const ProductsPageContent = () => {
@@ -33,6 +34,40 @@ const ProductsPageContent = () => {
                 columns={columns}
                 data={datas}
                 pageCount={1}
+                filterComponent={
+                    <>
+                        <Select >
+                            <SelectTrigger className="w-full max-w-48">
+                                <SelectValue placeholder="Filter by category" />
+                            </SelectTrigger>
+                            <SelectContent className="shadow-none">
+                                <SelectGroup>
+                                    <SelectLabel>Category</SelectLabel>
+                                    <SelectItem value="apple">Chicken</SelectItem>
+                                    <SelectItem value="banana">Burger</SelectItem>
+                                    <SelectItem value="banana">Fries</SelectItem>
+                                    <SelectItem value="banana">Drink</SelectItem>
+                                    <SelectItem value="banana">Dessert</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                        <Select>
+                            <SelectTrigger className="w-full max-w-48">
+                                <SelectValue placeholder="Filter by statuss" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>status</SelectLabel>
+                                    <SelectItem value="apple">Available</SelectItem>
+                                    <SelectItem value="banana">Unavailable</SelectItem>
+                                    <SelectItem value="banana">Hot Item</SelectItem>
+                                    <SelectItem value="banana">New Arrival</SelectItem>
+                                    <SelectItem value="banana">On Sale</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </>
+                }
 
 
             />
